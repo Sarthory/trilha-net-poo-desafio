f6ff7cfcac7b3eff2a588fcb63b1444d1d119dd4
Felipe Sartori <felipe@sartori.app>
2025-04-11 13:59:42 -0300

    Ajustando métodos e propriedades

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123", "3310", "123321", 256);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Smartphone iPhone = new Iphone("666", "15 Pro", "545897", 128);
+iPhone.ReceberLigacao();
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Instagram");
